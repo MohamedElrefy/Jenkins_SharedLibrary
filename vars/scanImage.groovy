c73213c8c0a337c5d08ac5d199f3fcb547a2d050
mohamed_elrefy <elrefymohamed2002@gmail.com>
2025-11-06 01:59:52 +0200

    slib

--- vars/scanImage.groovy
+++ vars/scanImage.groovy
@@ -5,10 +5,10 @@ def call(String imageName, String buildNumber) {
             sh '''
                 if ! command -v trivy &> /dev/null; then
                     echo "Installing Trivy..."
-                    apt-get update -y && apt-get install -y wget
-                    wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor > /usr/share/keyrings/trivy.gpg
+                    sudo apt-get update -y && apt-get install -y wget
+                    sudo wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor > /usr/share/keyrings/trivy.gpg
                     echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb stable main" | tee /etc/apt/sources.list.d/trivy.list
-                    apt-get update -y && apt-get install -y trivy
+                    sudo apt-get update -y && apt-get install -y trivy
                 fi
             '''
             sh "trivy image --severity HIGH,CRITICAL --exit-code 1 ${imageName}:${buildNumber} || echo '⚠️ Vulnerabilities found, review scan results above.'"
