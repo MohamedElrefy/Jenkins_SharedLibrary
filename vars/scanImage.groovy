76bde2415ca6a0cbb8ed4bda4f9f626f03482e0b
mohamed_elrefy <elrefymohamed2002@gmail.com>
2025-11-06 02:40:08 +0200

    slibb

--- vars/scanImage.groovy
+++ vars/scanImage.groovy
@@ -1,19 +1,13 @@
 def call(String imageName, String buildNumber) {
     stage('Scan Image') {
-        agent {
-            docker {
-                image 'aquasec/trivy:latest'
-                args '-v /var/run/docker.sock:/var/run/docker.sock'
-            }
-        }
-        steps {
-            script {
-                echo "🔍 Scanning Docker image for vulnerabilities..."
-                sh """
-                    trivy image --severity HIGH,CRITICAL --exit-code 1 ${imageName}:${buildNumber} \
-                    || echo '⚠️ Vulnerabilities found — review scan results above.'
-                """
-            }
-        }
+        echo "🔍 Scanning Docker image for vulnerabilities..."
+
+        // Run Trivy inside Docker
+        sh """
+            docker run --rm \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            aquasec/trivy:latest image --severity HIGH,CRITICAL --exit-code 1 ${imageName}:${buildNumber} \
+            || echo '⚠️ Vulnerabilities found — review scan results above.'
+        """
     }
 }
